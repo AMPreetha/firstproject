5b51149b2f82aea9476a6d4f46e6b7f578dd2983
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-08 10:56:11 +0530

    Restaurant apps

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import {Fragment} from 'react';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Layout/Header";
+import MealList from "./components/Meals/MealList";
+import MealsSummary from "./components/Meals/MealsSummary";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/Cart/CartProvider";
+const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-function App() {
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <Fragment>
-    <Header></Header>
-    <main>
-      <Meals/>
-    </main>
-    </Fragment>
-
-    
-   
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <MealsSummary />
+      <MealList />
+    </CartProvider>
   );
-}
+};
 
 export default App;
